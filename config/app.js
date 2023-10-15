fe3aca942fbc0f790e53dda47e1584da91baab10
Annie021 <akaur966@my.centennialcollege.ca>
2023-10-14 23:45:21 -0400

    first commit

---
+++ config/app.js
@@ -0,0 +1,37 @@
+var createError = require('http-errors');
+var express = require('express');
+var logger = require('morgan');
+
+/*Defining Routes for every document */
+var indexRouter = require('../app/routes/index');
+var productsRouter = require('../app/routes/products');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+app.use('/api/products', productsRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  console.log(err);
+
+  // Send the error response
+  res.status(err.status || 500);
+  res.json({
+    success: false,
+    message: err.message
+  });
+
+});
+
+module.exports = app;
---
+++ config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    dbUrl: 'mongodb://AnmoljeetKaur:Gwagonsixty3@@localhost:27017/DressStore',
+    // Add other configuration options as needed
+  };
+  
